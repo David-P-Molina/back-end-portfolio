e0f08f9af6d931db529af7b745f4bad502f927a6
David Moina <davemolina777@gmail.com>
2022-07-22 13:33:41 -0500

    Change file type from string to document

--- schemas/testimonials.js
+++ schemas/testimonials.js
@@ -1,7 +1,7 @@
 export default {
     name: 'testimonials',
     title: 'Testimonials',
-    type: 'string',
+    type: 'document',
     fields: [
         {
             name: 'name',
@@ -17,11 +17,11 @@ export default {
             type: 'image',
             options: {
                 hotspot: true,
-            }
+            },
         },{
             name: 'feedback',
             title: 'Feedback',
             type: 'string'
         }
     ]
-},
+}
